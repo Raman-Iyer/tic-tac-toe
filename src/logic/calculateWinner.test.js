a52630ed0b105f7c2e7b24a0e17f0df9eb7246a1
RamanIyer <raman.iyer@deuexsolutions.com>
2020-09-17 16:29:30 +0530

    Added unit test for calculate winner function

---
+++ src/logic/calculateWinner.test.js
@@ -0,0 +1,26 @@
+import calculateWinner from './calculateWinner'
+
+test('Check if X wins', () => {
+    const winner = calculateWinner(['X', 'X', 'X', 'O', 'O', '-', '-', '-', '-'])
+    expect(winner).toBe('X')
+});
+
+test('Check if X wins', () => {
+    const winner = calculateWinner(['X', 'O', 'X', 'O', 'O', 'X', 'X', 'X', 'O'])
+    expect(winner).toBeNull()
+});
+
+test('Check if X wins', () => {
+    const winner = calculateWinner(['O', 'X', 'O', 'X', 'O', 'X', 'O', '-', '-'])
+    expect(winner).toBe('O')
+});
+
+test('Check if X wins', () => {
+    const winner = calculateWinner(['X', '-', 'O', '-', 'X', 'O', 'X', 'O', 'X'])
+    expect(winner).toBe('X')
+});
+
+test('Check if X wins', () => {
+    const winner = calculateWinner(['O', 'O', 'X', 'X', 'O', 'O', 'O', 'X', 'X'])
+    expect(winner).toBeNull()
+});
