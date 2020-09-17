6dd26d0dc4ef312560260869d2309fb59d33f7cf
RamanIyer <raman.iyer@deuexsolutions.com>
2020-09-17 16:42:28 +0530

    Changed the board logic to use map and added CSS to handle the board design

--- src/components/Board.js
+++ src/components/Board.js
@@ -10,40 +10,16 @@ function Board(props) {
     // Add all the boxes with an index to keep track of the box that was click
     return (
         <div className="board">
-            {/* Map the boxList prop to set the corresponding values in each box */}
-            <div className="board-row">
-                <Box xO={boxList[0]} onClick={() => {
-                    onClick(0)
-                }} />
-                <Box xO={boxList[1]} onClick={() => {
-                    onClick(1)
-                }} />
-                <Box xO={boxList[2]} onClick={() => {
-                    onClick(2)
-                }} />
-            </div>
-            <div className="board-row">
-                <Box xO={boxList[3]} onClick={() => {
-                    onClick(3)
-                }} />
-                <Box xO={boxList[4]} onClick={() => {
-                    onClick(4)
-                }} />
-                <Box xO={boxList[5]} onClick={() => {
-                    onClick(5)
-                }} />
-            </div>
-            <div className="board-row">
-                <Box xO={boxList[6]} onClick={() => {
-                    onClick(6)
-                }} />
-                <Box xO={boxList[7]} onClick={() => {
-                    onClick(7)
-                }} />
-                <Box xO={boxList[8]} onClick={() => {
-                    onClick(8)
-                }} />
-            </div>
+            {
+                // Map the boxList prop to set the corresponding values in each box
+                boxList.map((box, i) => {
+                    return (
+                        <Box key={i} xO={box} onClick={() => {
+                            onClick(i)
+                        }} />
+                    )
+                })
+            }
         </div>
     )
 }
