2fe3c17a9e4c113801e454a333eeda7674accf0b
RamanIyer <raman.iyer@deuexsolutions.com>
2020-09-17 14:51:59 +0530

    Winner calculation has been commented properly

--- src/logic/calculateWinner.js
+++ src/logic/calculateWinner.js
@@ -1,4 +1,5 @@
 function calculateWinner(boxList) {
+    // All possible continous three boxes are the win conditions
     const winConditions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -10,6 +11,8 @@ function calculateWinner(boxList) {
         [2, 4, 6]
     ]
 
+    // Iterate over every array element and check if the the boxes contain the same symbol in them
+    // if so then return the symbol if not then return null
     for (let i = 0; i < winConditions.length; i++) {
         const [box1,box2,box3] = winConditions[i]
         if (boxList[box1] !== "-" && boxList[box1] === boxList[box2] && boxList[box1] === boxList[box3]){
@@ -19,4 +22,5 @@ function calculateWinner(boxList) {
     return null
 }
 
+// Export the function which we can use in the App component
 export default calculateWinner
