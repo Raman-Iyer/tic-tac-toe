2c7f1c51a73874e46057403b08a26d00951f8c29
RamanIyer <raman.iyer@deuexsolutions.com>
2020-09-17 15:57:53 +0530

    Added draw conditions

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   // useState Hooks
   const [boxList, setBoxList] = useState(Array(9).fill("-"))
   const [isXTurn, setIsXTurn] = useState(true)
+  const [steps, setSteps] = useState(0)
 
   // Winner keeps track of winner and xO keeps track of the what to put
   const winner = calculateWinner(boxList); // Need to add logic to calculate winner
@@ -28,14 +29,15 @@ function App() {
     // Set the new boxList and change the turn
     setBoxList(newBoxList)
     setIsXTurn(!isXTurn)
-
+    setSteps(steps + 1)
   }
 
   // Reset the state to the start of the game
-  function handleReset(){
+  function handleReset() {
     setBoxList(Array(9).fill("-"))
     // Last game's winner moves first
     setIsXTurn(winner === "X" ? true : false)
+    setSteps(0)
   }
 
   return (
@@ -43,9 +45,9 @@ function App() {
       <h1>Tic-Tac-Toe</h1>
       <Board boxList={boxList} onClick={handleClick} />
       {/* On basis of winner or turn display appropriate message */}
-      <h3>{winner ? `Winner Is ${winner}` : `${xO}'s Turn`}</h3>
+      <h3>{winner ? `Winner Is ${winner}` : steps === 9 ? "Game is a draw!" : `${xO}'s Turn`}</h3>
       {/* Reset the state again after the game is completed */}
-      <button onClick={handleReset} style={{display: winner ? "block" : "none"}}>Reset!</button>
+      <button onClick={handleReset} style={{ display: winner || steps === 9 ? "block" : "none" }}>Reset!</button>
     </div>
   );
 }
