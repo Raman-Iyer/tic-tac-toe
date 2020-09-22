7242a6374a96a5b611b56d0dc4d3b65035079e84
RamanIyer <raman.iyer@deuexsolutions.com>
2020-09-22 18:43:50 +0530

    Updates

---
+++ precache-manifest.0587b7c84926867d6353310936fcd85e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd3c4340afc4f29849d6a02e7851424b",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "8a489e4b8eee715b5e40",
+    "url": "/tic-tac-toe/static/css/main.14bab8c2.chunk.css"
+  },
+  {
+    "revision": "d4beeb07b80aeb6b3f4c",
+    "url": "/tic-tac-toe/static/js/2.f47b7d6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.f47b7d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a489e4b8eee715b5e40",
+    "url": "/tic-tac-toe/static/js/main.f6eb6b2d.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
